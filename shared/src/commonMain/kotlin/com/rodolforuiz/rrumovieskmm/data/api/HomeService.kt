7ff6657143c5976eae73ee1e36ebf82e572e2633
Rodolfo RUiz <rodolforuyz@gmail.com>
2024-05-25 02:16:48 -0300

    adicionado filme url

--- shared/src/commonMain/kotlin/com/rodolforuiz/rrumovieskmm/data/api/HomeService.kt
+++ shared/src/commonMain/kotlin/com/rodolforuiz/rrumovieskmm/data/api/HomeService.kt
@@ -8,7 +8,12 @@ class HomeService {
     private val client = HttpClient()
 
     suspend fun greeting(): String {
-        val response = client.get("https://ktor.io/docs/")
+        val response = client.get("https://api.themoviedb.org/3/movie/popular"){
+            url {
+                parameters.append("api_key", "e591023d8d396231d3045ea6341a6fd2")
+                parameters.append("language", "pt-BR")
+            }
+        }
         return response.bodyAsText()
     }
 }
